0988d8372737f0af1417b0bc72a5c259667a4b7a
Kayla <kayla6261@gmail.com>
2023-05-03 18:05:02 -0400

    Mayhem

---
+++ mayhem/fuzz_convert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import atheris
+import sys
+
+with atheris.instrument_imports():
+    from ansi2html import Ansi2HTMLConverter
+
+# Global converter
+conv = Ansi2HTMLConverter()
+
+def RandomString(fdp, min_len, max_len):
+  str_len = fdp.ConsumeIntInRange(min_len, max_len)
+  return fdp.ConsumeUnicodeNoSurrogates(str_len)
+
+def TestOneInput(data):
+    fdp = atheris.FuzzedDataProvider(data)
+
+    ansi = RandomString(fdp, 0, 100)
+    conv.convert(ansi)
+
+
+atheris.Setup(sys.argv, TestOneInput)
+atheris.Fuzz()
